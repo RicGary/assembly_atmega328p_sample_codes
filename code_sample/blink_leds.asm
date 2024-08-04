2311e95f4d924f19966e748bbc9e37339d0bab40
RicGary <ericnaiber@gmail.com>
2024-08-04 20:07:53 -0300

    desisto to nem ai

--- code_sample/blink_leds.asm
+++ code_sample/blink_leds.asm
@@ -2,8 +2,7 @@
 
 .org 0x0000              ; Define o endereço de início do programa
 
-.equ TIMER_VALUE_HIGH = 0x3D
-.equ TIMER_VALUE_LOW  = 0xB9
+.equ TIMER_VALUE = 15625
 
 ; Configura a pilha
 ldi r16, high(RAMEND) ; Carrega o byte mais significativo do endereço de RAMEND em r16
@@ -22,9 +21,9 @@ ldi r16, 0b00_00_00_00
 sts TCCR1A, r16
 ldi r16, 0b00_0_00_101  ; Prescaler de 1024, levando clock para 15.625 Hz
 sts TCCR1B, r16
-ldi r16, low(TIMER_VALUE_LOW)     ; Valor escolhido para cada ciclo levar 1s
+ldi r16, low(TIMER_VALUE)     ; Valor escolhido para cada ciclo levar 1s
 sts OCR1AL, r16
-ldi r16, high(TIMER_VALUE_HIGH)
+ldi r16, high(TIMER_VALUE)
 sts OCR1AH, r16
 
 ; Comeca a piscar os leds
@@ -33,9 +32,9 @@ main_loop:
     lds r16, TCNT1H
 
     ; Verifica se o timer chegou nos valores
-    cpi r16, high(TIMER_VALUE_HIGH)    ; Compara a parte alta do valor, se o resultado for zero aciona o flag (Z)
+    cpi r16, high(TIMER_VALUE)    ; Compara a parte alta do valor, se o resultado for zero aciona o flag (Z)
     brne main_loop
-    cpi r17, low(TIMER_VALUE_LOW)     ; Subtrai a parte low do valor da parte low do timer e levanta uma flag
+    cpi r17, low(TIMER_VALUE)     ; Subtrai a parte low do valor da parte low do timer e levanta uma flag
     brlo main_loop          ; Pula para loop se TCNT1L < low(15625) pois seta flag Carry 
 
     ; Altera o estado dos leds
