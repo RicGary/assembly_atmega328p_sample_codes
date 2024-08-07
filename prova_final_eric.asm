f6a837c1d65cdec7f962c29891162608d19954fc
RicGary <ericnaiber@gmail.com>
2024-08-07 20:58:59 -0300

    adicionando logica de dois ADC

--- prova_final_eric.asm
+++ prova_final_eric.asm
@@ -13,8 +13,10 @@
 ; FEITO
 ; - Realizar N (N>16)medidas consecutivas e realizar uma m�dia aritm�tica do valor. 
 
+; FEITO
 ; - Guardar o valor das �ltimas 124 m�dias na mem�ria.
 
+; 
 ; - Calcular um sinal de erro a partir da um valor de refer�ncia predeterminado no 
 ;   programa via uma porta anal�gica (0-5V)
 
@@ -36,14 +38,18 @@
 .include m328Pdef.inc
 .org 0x0000
 
-.def contador_medidas       = r20
-.def cte_medidas            = r11
+.def contador_medidas       = r21
+.def cte_medidas            = r10
 .def deslocamentos_dir      = r22   ; Deve ser N.medidas = 2 ^ deslocamentos_para_direita, nesse caso 16 = 2^4, entao 4 deslocamentos
-.def deslocamentos_dir_cte  = r12   ; Valor fixo de deslocamentos
-.def quantidade_medias      = r19   ; Valor responsavel por armazenar x medias
-.def quantidade_medias_cte  = r14   ; Cte var. anterior                                  
-.def soma_low               = r23  
-.def soma_high              = r24
+.def deslocamentos_dir_cte  = r11   ; Valor fixo de deslocamentos
+.def quantidade_medias      = r23   ; Valor responsavel por armazenar x medias
+.def quantidade_medias_cte  = r12   ; Cte var. anterior                                  
+.def soma_low               = r24  
+.def soma_high              = r25
+.def termometro             = r13
+.def potenciometro          = r14
+
+; Reg usados: r11, r12, r13, r14, r21, r22, r23, r24, r25
 
 ; Configura pilha 
 ldi    R16,low(RAMEND)
@@ -59,6 +65,10 @@ ldi deslocamentos_dir    , 4    ; r16/4
 mov deslocamentos_dir_cte, deslocamentos_dir
 mov contador_medidas     , r16  ; Contador do loop para armazenar medidas
 mov cte_medidas          , r16  ; Cte. do loop 
+ldi r16, 0b01_0_0_0000
+mov termometro           , r16
+ldi r16, 0b01_0_0_0001
+mov potenciometro        , r16
 ; Z parte alta, Y parte baixa
 ldi r30, low(0x0300)            ; Endereço inicial baixo para Z, Endereço maximo = Endereço inicial + 124 (7C)
 ldi r31, high(0x0300)           ; Endereço inicial alto  para Z
@@ -66,20 +76,26 @@ ldi r28, low(0x0400)            ; Endereço inicial baixo para Y
 ldi r29, high(0x0400)           ; Endereço inicial alto  para Y
 
 ; Configura o ADC -> termometro PC0
-ldi    R16,     0b01_0_0_0000   ; REFS1,REFS0 (01) = Vref igual ao Vcc, ADLAR (0) 8 bits ADCL 2 bits ADCH, MUX (0000) ADC0 porta PC0
-sts    ADMUX,   R16            
-ldi    R16,     0b1_1_1_0_0_101 ; ADEN (1) habilita ADC, ADSC (1) habilita para iniciar conversao, ADATE (1) conversao automatica, ADIF (0) e ADIE (0) nao serao utilizados, ADPSx (101) prescaler de 32
-sts    ADCSRA,  R16            
-ldi    R16,     0b0000_0_000    ; ACME (0) utiliza entrada padrao do ADC, ADTSx (000) ADC automaticamente inicia uma nova conversao assim que a anterior for feita
-sts    ADCSRB,  R16             ; Configura autotrigger em free running
+;ldi    R16,     0b01_0_0_0000   ; REFS1,REFS0 (01) = Vref igual ao Vcc, ADLAR (0) 8 bits ADCL 2 bits ADCH, MUX (0000) ADC0 porta PC0
+;sts    ADMUX,   R16            
+;ldi    R16,     0b1_0_0_0_0_101 ; ADEN (1) habilita ADC, ADSC () desabilita para iniciar conversao, ADATE (0) desabilita conversao automatica, ADIF (0) e ADIE (0) nao serao utilizados, ADPSx (101) prescaler de 32
+;sts    ADCSRA,  R16            
+;ldi    R16,     0b0000_0_000    ; ACME (0) utiliza entrada padrao do ADC, ADTSx (000) ADC automaticamente inicia uma nova conversao assim que a anterior for feita
+;sts    ADCSRB,  R16             ; Configura autotrigger em free running
+
+
+; Configurar o ADC -> potenciometro PC1
+;ldi    R16,     0b01_0_0_0001   ; REFS1,REFS0 (01) = Vref igual ao Vcc, ADLAR (0) 8 bits ADCL 2 bits ADCH, MUX (0001) ADC0 porta PC1
+;sts    ADMUX,   R16            
+;ldi    R16,     0b1_0_0_0_0_101 ; ADEN (1) habilita ADC, ADSC (0) desabilita para iniciar conversao, ADATE (0) desabilita conversao automatica, ADIF (0) e ADIE (0) nao serao utilizados, ADPSx (101) prescaler de 32
+;sts    ADCSRA,  R16            
+;ldi    R16,     0b0000_0_000    ; ACME (0) utiliza entrada padrao do ADC, ADTSx (000) ADC automaticamente inicia uma nova conversao assim que a anterior for feita
+;sts    ADCSRB,  R16             ; Configura autotrigger em free running
 
 loop_add:
-    lds r16, ADCH
-    lds r17, ADCL
-
-    ; Debug
-    ldi r16, 1
-    ldi r17, 1
+    ; Le primeiro o termometro
+    sts  ADMUX, termometro   ; Garante que sera mexido no pino PC0
+    call ler_adc
 
     add soma_low, r16           ; Adicionar parte baixa à soma
     adc soma_high, r17          ; Adicionar parte alta à soma com carry
@@ -100,8 +116,28 @@ store_media: ; Armazena as n medias nos registradores Y e Z, reinicia variaveis
     dec quantidade_medias
     brne loop_add
 
+calculo_erro:
+    ld r18, Z   ; Parte high da media do termometro
+    ld r19, Y   ; Parte low da media do termometro
+    sts ADMUX, potenciometro    ; Garante que o pino PC1 sera lido
+    call ler_adc
+
 memory_reset: ; Verifica se foram escritos 124 valores na memoria para comecar a sobrescrever
     ldi YL, low(0x0400)
     ldi ZL, low(0x0300)
     mov quantidade_medias, quantidade_medias_cte
     rjmp loop_add
+
+ler_adc:
+    ldi r16, 0b1_1_000101   ; Inicia conversao
+    sts  ADCSRA, r16        ; Inicia uma conversao
+    
+    ; Nao sei como arrumar isso
+    ;sbis ADCSRA, 4          ; Espera a conversão de ADC0 terminar 
+    ;rjmp ler_adc
+    
+    ldi r16, 0b1_1_000101   ; Baixa flag de conversao
+    sts  ADCSRA, r16       ; Nao tenho certeza se baixa a flag apos a conversao automaticamente
+    lds  r16, ADCH
+    lds  r17, ADCL
+    ret
