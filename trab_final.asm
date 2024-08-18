132390112921dbc3c8ae6b93822201f1f13c815a
RicGary <ericnaiber@gmail.com>
2024-08-17 21:08:35 -0300

    agora vai

--- trab_final.asm
+++ trab_final.asm
@@ -1,6 +1,6 @@
 .include "m328pdef.inc"  ; Inclui o arquivo de definicão do ATmega328P
-
 .org   0x0000              ; Define o endereco de inicio do programa
+rjmp   config
 
 ; Interrupts
 .org   0x0002      ; Jogador 1 
@@ -8,68 +8,69 @@ rjmp   botao_j1
 .org   0x0004      ; Jogador 2
 rjmp   botao_j2
 
-; Definindo variaveis
-.def   pontuacao_j1 = r20
-.def   pontuacao_j2 = r21
-.def   vitorias_j1  = r22
-.def   vitorias_j2  = r23
-.def   botao_reset  = r24
-.def   num_final    = r25
-.def   is_pressed   = r26
-.def   j1_pressed   = r27
-.def   j2_pressed   = r28
+config:
+    ; Definindo variaveis
+    .def   pontuacao_j1 = r20
+    .def   pontuacao_j2 = r21
+    .def   vitorias_j1  = r22
+    .def   vitorias_j2  = r23
+    .def   botao_reset  = r24
+    .def   num_final    = r25
+    .def   is_pressed   = r26
+    .def   j1_pressed   = r27
+    .def   j2_pressed   = r28
 
-; Configura a pilha
-ldi   r16, low(RAMEND)  ; Carrega o byte menos significativo do endereco de RAMEND em r16
-out   SPL, r16          ; Armazena o byte menos significativo do endereco de RAMEND no registrador SPL
-ldi   r16, high(RAMEND) ; Carrega o byte mais significativo do endereco de RAMEND em r16
-out   SPH, r16          ; Armazena o byte mais significativo do endereco de RAMEND no registrador SPH
+    ; Configura a pilha
+    ldi   r16, low(RAMEND)  ; Carrega o byte menos significativo do endereco de RAMEND em r16
+    out   SPL, r16          ; Armazena o byte menos significativo do endereco de RAMEND no registrador SPL
+    ldi   r16, high(RAMEND) ; Carrega o byte mais significativo do endereco de RAMEND em r16
+    out   SPH, r16          ; Armazena o byte mais significativo do endereco de RAMEND no registrador SPH
 
-; Configura os pinos  Gasta mais linhas e energia mas prefiro fazer assim para deixar facil de ler as portas
-cbi   DDRD, PD2         ; Configura PD2 como entrada (BOTAO) -> Jogador 1
-cbi   DDRD, PD3         ; Configura PD3 como entrada (BOTAO) -> Jogador 2
-cbi   DDRB, PD4         ; Configura PD4 como entrada (BOTAO) -> Reset da rodada
-ser   r16               ; Seta todos os bits como 1
-out   PORTB, r16        ; Ativa o resistor pull-up no PBx
+    ; Configura os pinos  Gasta mais linhas e energia mas prefiro fazer assim para deixar facil de ler as portas
+    cbi   DDRD, PD2         ; Configura PD2 como entrada (BOTAO) -> Jogador 1
+    cbi   DDRD, PD3         ; Configura PD3 como entrada (BOTAO) -> Jogador 2
+    cbi   DDRB, PD4         ; Configura PD4 como entrada (BOTAO) -> Reset da rodada
+    ser   r16               ; Seta todos os bits como 1
+    out   PORTB, r16        ; Ativa o resistor pull-up no PBx
 
-sbi   DDRD, PB0             ; Configura PB0 como saida -> Jogador 1 vence
-sbi   DDRD, PB1             ; Configura PB1 como saida -> Jogador 2 vence
-clr	  R16				    ; Limpa R16
-out	  PORTD,R16	            ; limpa a porta D para saida - estado inicial desliga led
+    sbi   DDRD, PB0             ; Configura PB0 como saida -> Jogador 1 vence
+    sbi   DDRD, PB1             ; Configura PB1 como saida -> Jogador 2 vence
+    clr	  R16				    ; Limpa R16
+    out	  PORTD,R16	            ; limpa a porta D para saida - estado inicial desliga led
 
-; Configura interrupt para os jogadores
-ldi   r16  , 0b00001010     ; ISC01 = 1, ISC11 = 1 para borda de descida, ISC00 = 0, ISC10 = 0
-sts   EICRA, r16            ; Define borda de descida para INT0 e INT1
-ldi   r16  , 0b00000011     ; 0b00000011 corresponde a INT1 e INT0 habilitados
-out   EIMSK, r16            ; Ativa as interrupcões para INT0 e INT1
-sei                         ; Habilita interrupcões globais
+    ; Configura interrupt para os jogadores
+    ldi   r16  , 0b00001010     ; ISC01 = 1, ISC11 = 1 para borda de descida, ISC00 = 0, ISC10 = 0
+    sts   EICRA, r16            ; Define borda de descida para INT0 e INT1
+    ldi   r16  , 0b00000011     ; 0b00000011 corresponde a INT1 e INT0 habilitados
+    out   EIMSK, r16            ; Ativa as interrupcões para INT0 e INT1
+    sei                         ; Habilita interrupcões globais
 
-; Configura parte do ADC
-ldi   r16  , 0b01_0_0_0000  ; Move valor para r16
-sts   ADMUX, r16            ; Garante que sera lido no pino PC0/A0
+    ; Configura parte do ADC
+    ldi   r16  , 0b01_0_0_0000  ; Move valor para r16
+    sts   ADMUX, r16            ; Garante que sera lido no pino PC0/A0
 
-; Configura timer do debouncing 
-ldi   R16   , 0b00_00_00_00 ; Move o valor para r16
-out   TCCR0A, R16           ; Configura modo normal = delay
-ldi   R16   , 0b00_00_0_010 ; Move o valor para r16
-out   TCCR0B, R16           ; Configura clk com prescaler de PS8 = 010
+    ; Configura timer do debouncing 
+    ldi   R16   , 0b00_00_00_00 ; Move o valor para r16
+    out   TCCR0A, R16           ; Configura modo normal = delay
+    ldi   R16   , 0b00_00_0_010 ; Move o valor para r16
+    out   TCCR0B, R16           ; Configura clk com prescaler de PS8 = 010
 
-; Configura timer do LED
-; Considerando apenas a parte alta, quero que o led fique ligado +- 5s
-ldi   R16, 0b00000000          ; Configura o modo do Timer 1 para CTC (WGM12 = 1)
-sts   TCCR1A, R16              ; Passa o valor para o TCCR1A
-ldi   R16, 0b00001100          ; CTC com prescaler de 256 (CS12 = 1)
-sts   TCCR1B, R16              ; Passa o valor para o TCCR1B
+    ; Configura timer do LED
+    ; Considerando apenas a parte alta, quero que o led fique ligado +- 5s
+    ldi   R16, 0b00000000          ; Configura o modo do Timer 1 para CTC (WGM12 = 1)
+    sts   TCCR1A, R16              ; Passa o valor para o TCCR1A
+    ldi   R16, 0b00001100          ; CTC com prescaler de 256 (CS12 = 1)
+    sts   TCCR1B, R16              ; Passa o valor para o TCCR1B
 
-; Definindo constantes
-clr   pontuacao_j1
-clr   pontuacao_j2
-clr   vitorias_j1
-clr   vitorias_j2
+    ; Definindo constantes
+    clr   pontuacao_j1
+    clr   pontuacao_j2
+    clr   vitorias_j1
+    clr   vitorias_j2
 
 wait_button:
     ser    botao_reset       ; Seta se for parte da rotina de reset
-    sbis   PIND, 4           ; Confere se o PD4 esta setado
+    sbic   PIND, 4           ; Confere se o PD4 esta setado
     rjmp   buttom_debouncing ; Vai para o filtro
     rjmp   wait_button       ; Fica em loop
 
